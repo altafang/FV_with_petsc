793f42e6313691797b94d3b7d460acd1024b213c
Alta Fang <alta@princeton.edu>
2017-12-16 20:18:05 -0500

    fixed a bug in the 2D poisson solver

--- test/test_2D/run_test.py
+++ test/test_2D/run_test.py
@@ -4,21 +4,34 @@ import subprocess
 from make_inputs import make_sigma, make_source
 from validate_results import validate_results
 
-command = "cp ../../bin/solve_poisson_2D ."
-subprocess.call(command, shell=True)
-print command
+def run_test(variation='X'):
+    
+    if not ((variation == 'X') or (variation == 'Y')):
+        print "Invalid dimension of variation, must be X or Y"
+        return
+        
+    # Modify input.txt file
+    subprocess.call("cp input_variation" + variation + ".txt input.txt", shell=True)
 
-# Make inputs
-print "Generating input files..."
-make_sigma(plot=False)
-make_source()
+    command = "cp ../../bin/solve_poisson_2D ."
+    subprocess.call(command, shell=True)
+    print command
 
-# Run solve
-command = "mpiexec -np 1 ./solve_poisson_2D"
-print command
-print "Solving..."
-subprocess.call(command, shell=True)
+    # Make inputs
+    print "Generating input files..."
+    make_sigma(plot=False)
+    make_source()
 
-# Validate
-print "Validating..."
-validate_results(plot=False)
+    # Run solve
+    command = "mpiexec -np 1 ./solve_poisson_2D"
+    print command
+    print "Solving..."
+    subprocess.call(command, shell=True)
+
+    # Validate
+    print "Validating..."
+    validate_results(plot=False, variation=variation)
+    
+if __name__ == '__main__':
+    run_test(variation='X')
+    run_test(variation='Y')
--- test/test_2D/validate_results.py
+++ test/test_2D/validate_results.py
@@ -10,7 +10,11 @@ import sys
 sys.path.insert(0, '../')
 from tools import *
 
-def validate_results(path = "", plot=True):
+def validate_results(path="", plot=True, variation='X'):
+
+    if not ((variation == 'X') or (variation == 'Y')):
+        print "Invalid dimension of variation, must be X or Y"
+        return
 
     # Read parameters from the input file
     param_dict = extract_params(path)
@@ -23,8 +27,13 @@ def validate_results(path = "", plot=True):
     phi = read_hdf5(path + "phi.h5", dims)
 
     # Check that it is linear
-    theoretical_1D = numpy.linspace(1, 0, NX+2)[1:-1]
-    theoretical_2D = numpy.broadcast_to(theoretical_1D.reshape(1, NX), (NY, NX))
+    if variation == 'X':
+        theoretical_1D = numpy.linspace(1, 0, NX+2)[1:-1]
+        theoretical_2D = numpy.broadcast_to(theoretical_1D.reshape(1, NX), (NY, NX))
+    else:
+        theoretical_1D = numpy.linspace(1, 0, NY+2)[1:-1]
+        theoretical_2D = numpy.broadcast_to(theoretical_1D.reshape(NY, 1), (NY, NX))
+            
     # Assert if error is too large
     try:
         rtol = 1.e-3
@@ -40,11 +49,15 @@ def validate_results(path = "", plot=True):
         pyplot.show()
         
         # Check that it is linear
-        pyplot.plot(range(NX), theoretical_1D, 'r')
-        pyplot.plot(range(NX), phi[NY/2,:], 'k-o')
+        if variation == 'X':
+            pyplot.plot(range(NX), theoretical_1D, 'r')
+            pyplot.plot(range(NX), phi[NY/2,:], 'k-o')
+        else:
+            pyplot.plot(range(NY), theoretical_1D, 'r')
+            pyplot.plot(range(NY), phi[:, NX/2], 'k-o')        
         pyplot.savefig("slice1D_middle.png")
         pyplot.show()
 
 if __name__ == '__main__':
-    validate_results(plot=False)
+    validate_results(plot=True, variation='Y')
 
